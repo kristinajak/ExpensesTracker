15db4e6a608925d188744d1c46848a3629571650
kristinajak <kristina.jakubone@gmail.com>
2023-08-09 10:26:25 +0300

    moving from local to git

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import Card from "../UI/Card";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const selectedYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onSelectedYear={selectedYearHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    // Calling a function we receive via props and assigning a value to it
+    // (props from parent element. In this case from App.js)
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
